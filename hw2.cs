eb834dec749316efd849fadb82c0d3a7d452eb8d
ASFedotov <asfedotov1988as@gmail.com>
2023-01-25 23:35:02 +0500

    add new fail

--- hw2.cs
+++ hw2.cs
@@ -23,7 +23,7 @@ string [] ArrayString(int num){
 void ShowArray(string [] array){
     
     for (int i = 0; i < array.Length; i++){
-        Console.Write($" {array[i]}");
+        Console.Write($"'{array[i]}'  ");
     }
     Console.WriteLine();
 }
@@ -32,7 +32,7 @@ void MaxNum3(string[] array){
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
-            Console.Write($"{array[i]}   ");     
+            Console.Write($"'{array[i]}'  ");     
     }
 }
 
